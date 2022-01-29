69882966a1f98745fadf813bb7d79dd6bd30790c
L.Bednarek <bednarek.lukasz@gmail.com>
2022-01-29 14:27:11 +0100

    feat: basic dialog (wip)

--- src/components/order/BasicDialog.js
+++ src/components/order/BasicDialog.js
@@ -11,11 +11,16 @@ import { useTranslation } from 'react-i18next';
 //UI
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Grid from '@material-ui/core/Grid';
+
+const placeholderImg = 'https://via.placeholder.com/400?text=No%20image';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,6 +33,13 @@ const useStyles = makeStyles((theme) => ({
     top: theme.spacing(1),
     color: theme.palette.grey[500],
   },
+  fillWidth: {
+    width: '100%',
+  },
+  centerContent: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
 }));
 
 const BasicDialog = ({ product, isOpen, closeFn }) => {
@@ -35,14 +47,32 @@ const BasicDialog = ({ product, isOpen, closeFn }) => {
   const { t } = useTranslation();
 
   return (
-    <Dialog key={product.id} open={isOpen ?? false} onClose={closeFn}>
-      <DialogTitle id='alert-dialog-title'>
-        {`Dialog for product id: ${product.id}`}
-      </DialogTitle>
+    <Dialog
+      key={product.id}
+      fullWidth={true}
+      maxWidth={'md'}
+      open={isOpen ?? false}
+      onClose={closeFn}
+    >
       <DialogContent>
         <DialogContentText id='alert-dialog-description'>
-          Let Google help apps determine location. This means sending anonymous
-          location data to Google, even when no apps are running.
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6}>
+              <Container>
+                <img
+                  src={product.imageUrl ?? placeholderImg}
+                  alt=''
+                  style={{ width: '100%' }}
+                />
+              </Container>
+              <Box className={classes.centerContent}>
+                <Button>Pick Files</Button>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              B
+            </Grid>
+          </Grid>
         </DialogContentText>
       </DialogContent>
       <DialogActions>
