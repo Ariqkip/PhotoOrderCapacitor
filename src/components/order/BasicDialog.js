a0afc84136c6d4f5043beb1b94235d8190974200
L.Bednarek <bednarek.lukasz@gmail.com>
2022-02-01 20:16:37 +0100

    feat: add photo basic dialog (wip)

--- src/components/order/BasicDialog.js
+++ src/components/order/BasicDialog.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 
 //Components
+import RoundButton from './../core/RoundButton';
 
 //Hooks
 import { useTranslation } from 'react-i18next';
@@ -19,6 +20,8 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Grid from '@material-ui/core/Grid';
+import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
+import Typography from '@material-ui/core/Typography';
 
 const placeholderImg = 'https://via.placeholder.com/400?text=No%20image';
 
@@ -38,13 +41,28 @@ const useStyles = makeStyles((theme) => ({
   },
   centerContent: {
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  spaceBetweenContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  title: {
+    color: '#3A3A3A',
+    fontWeight: 600,
+    marginBottom: '20px',
+  },
+  description: {
+    marginBottom: '20px',
   },
 }));
 
 const BasicDialog = ({ product, isOpen, closeFn }) => {
   const classes = useStyles();
   const { t } = useTranslation();
+  console.log('%cLQS logger: ', 'color: #c931eb', { product });
 
   return (
     <Dialog
@@ -66,11 +84,31 @@ const BasicDialog = ({ product, isOpen, closeFn }) => {
                 />
               </Container>
               <Box className={classes.centerContent}>
-                <Button>Pick Files</Button>
+                <RoundButton>
+                  <Box className={classes.centerContent}>
+                    <AddPhotoAlternateIcon />
+                    <span>{t('Pick files')}</span>
+                  </Box>
+                </RoundButton>
               </Box>
             </Grid>
             <Grid item xs={12} md={6}>
-              B
+              <Box className={classes.spaceBetweenContent}>
+                <Box>
+                  <Typography variant='h6' className={classes.title}>
+                    {product.name}
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant='h6' className={classes.title}>
+                    {product.price} €
+                  </Typography>
+                </Box>
+              </Box>
+              <Typography variant='body2' className={classes.description}>
+                {product.description}
+              </Typography>
+              image list
             </Grid>
           </Grid>
         </DialogContentText>
